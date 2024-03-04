cbdab839d828fac4ea7a78f06d9b97008aa936f8
6301287177 <147589128+6301287177@users.noreply.github.com>
2024-03-04 14:54:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function change(){
+    document.getElementById("shope1").style.border="double 10px purple";
+}
+    function change1(){
+         document.getElementById("shope2").style.border="double 10px green";
+        
+    }
+ function change2(){
+         document.getElementById("shope3").style.border="double 10px yellow";
+        
+    }
+ function change3(){
+         document.getElementById("shope4").style.border="double 10px orange ";
+        
+    }
+
+
